e64fe5bf30b85f02c5365cfc38185ab69094b1e2
unknown <rutvikokate2610@gmail.com>
2019-12-03 18:37:42 +0530

    changed view

--- src/components/Weather/wea.jsx
+++ src/components/Weather/wea.jsx
@@ -37,7 +37,7 @@ export class Weather2 extends Component {
         }
         return(
             <ul>
-              {suggestion.map((item)=><li className="tag is-success is-rounded" style={{cursor:'pointer'}}onClick={()=>this.suggestionselected(item)}>{item}</li>)}
+              {suggestion.map((item)=><li className="tag is-success is-medium" style={{cursor:'pointer'}}onClick={()=>this.suggestionselected(item)}>{item}</li>)}
           </ul>
         );
     }
@@ -52,6 +52,9 @@ export class Weather2 extends Component {
         }))
         console.log(value)
     }
+
+
+  
 
   render() {
     const {text}=this.state;
@@ -72,13 +75,16 @@ export class Weather2 extends Component {
                             
                            
                             <form action="" onSubmit={this.props.loadweather}>
-                            <div className="title is-5">Your city and country:</div>
-                            <div className="field">
-                            <div className="control">
+                            <div className="title is-4">Your city and country:</div>
+                             <div className="field has-addons">
+                            <div className="control has-icons-right is-expanded">
                                 <input type="text" value={text} onChange={this.ontextchange} className="input is-success " name="country" autoComplete="off" placeholder="Country"/>
                                 {this.rendersuggestions()}
+                    
                             </div>
                             </div>
+                                    
+
                             <div className="field">
                                 <div className="control">
                                     <input type="text" className="input is-success" name="city" autoComplete="off" placeholder="City"/>
@@ -86,7 +92,7 @@ export class Weather2 extends Component {
                                 </div>
                             </div>
                             <div className="field is-grouped is-grouped-centered">
-                                <button className="button is-success is-outlined" name="submit" >Get Weather</button>
+                                <button className="button  is-success "   id="button" name="submit" ><span className="icon"><i class="fas fa-arrow-circle-down"></i></span><span>Get Weather</span></button>
                             </div>
                             </form>
                         </div>
@@ -95,59 +101,59 @@ export class Weather2 extends Component {
                     <div className="column right">
                         
                        <div className="box">
-                       <h1>{this.props.length}</h1>
-                       {this.props.city?<h1 className="title is-3">Weather in {this.props.city} </h1>:<h1 className="title is-3" style={{color:'#e7e7e7'}}>City,Country</h1>}
+                       {this.props.city?<h1 className="title is-4">Weather in {this.props.city} </h1>:<h1 className="title is-4" style={{color:'#e7e7e7'}}>City,Country</h1>}
                        <hr/>
-                       <div className="columns weather-bar">
-                            <div className="column is-one-third" style={{background:''}}>
-                            <div className="field is-grouped is-grouped-centered">
+                       <div className="columns weather-bar" style={{marginBottom:'0px'}}>
+                            <div className="column is-one-third" style={{background:'',padding:'0px',textAlign:'center'}}>
+                            <div className="field is-grouped is-grouped-centered" style={{}}>
                             <h1 className="title is-1">
-                                {this.props.weatherIcon?<img  className="image is-110x110" src={`http://openweathermap.org/img/wn/${this.props.weatherIcon}@2x.png`} alt=""/>:<span>-</span>}
+                                {this.props.weatherIcon?<img  className="image is-100x100" src={`http://openweathermap.org/img/wn/${this.props.weatherIcon}@2x.png`} alt=""/>:<span className="blank">-</span>}
                             </h1>
+                           
                             </div>
+                            <p className="title is-4" style={{textTransform:'capitalize',paddingTop:'5px'}}>{this.props.description?<span>{this.props.description}</span>:<span>{null}</span>}</p>
                             </div>
 
                             <div className="column"  style={{background:''}}>
                                 <div className="content">
-                                    <nav className="level is-mobile">
+                                    <nav className="level is-mobile" style={{background:'',marginBottom:'20px'}}>
                                         <div className="level-item has-text-centered">
                                             <div>
-                                            <p className="heading">Temperature</p>
-                                            {this.props.temp_celcius?<p className="title">{this.props.temp_celcius}&deg;C</p>:<p className="title">-</p>}
+                                            <p className="heading">Now</p>
+                                            {this.props.temp_celcius?<p className="title is-1">{this.props.temp_celcius}&deg;C</p>:<p className="title blank">-</p>}
                                             </div>
                                             
                                         </div>
-                                        <div className="level-item has-text-centered">
-                                            <div>
-                                            <p className="heading">Minimum</p>
-                                            {this.props.temp_min?<p className="title">{this.props.temp_min}&deg;C</p>:<p className="title">-</p>}
-                                            </div>
-                                        </div>
-                                        <div className="level-item has-text-centered">
-                                            <div>
-                                            <p className="heading">Maximum</p>
-                                            {this.props.temp_max?<p className="title">{this.props.temp_max}&deg;C</p>:<p className="title">-</p>}
-                                            </div>
-                                        </div>
+                                       
                                     </nav>
+                                    <nav className="level is-mobile" style={{background:'',textAlign:'center'}}>
+                                        <div className="level-item" style={{background:'',display:'flex',justifyContent:'flex-end',}}>
+                                            <div style={{background:'',paddingRight:'50px'}}>
+                                            <p className="heading">low</p>
+                                            {this.props.temp_min?<p className="subtitle is-size-4">{this.props.temp_min}&deg;C</p>:<p className="title blank">-</p>}
+                                            </div>
+                                        </div>
+                                        <div className="level-item" style={{background:'',display:'flex',justifyContent:'flex-start',paddingLeft:'50px'}} >
+                                            <div>
+                                            <p className="heading">high</p>
+                                            {this.props.temp_max?<p className="subtitle is-size-4">{this.props.temp_max}&deg;C</p>:<p className="title blank">-</p>}
+                                            </div>
+                                        </div>
+                                        </nav>
                                 </div>
                             </div>
                         </div>
                        
                         <div className="  description ">
                             <div className="left">
-                            <p><span className="heading "><i className="wi wi-strong-wind is-size-5"/> wind speed: </span>{this.props.wind_speed?<span>{this.props.wind_speed} m/s</span>:<i className="wi wi-na is-size-4"/>}</p> 
-                               
-                          
-                               <p><span className="heading has-text-info"><i className="fas fa-info-circle is-size-6"/> Status: </span>{this.props.description?<span>{this.props.description}</span>:<span><i className="wi wi-na is-size-4"/></span>}</p>
-                               
+                            <p><span className="heading "><i className="wi wi-strong-wind is-size-5"/> wind speed: </span>{this.props.wind_speed?<span>{this.props.wind_speed} m/s</span>:<i className="wi wi-na is-size-4 blank"/>}</p> 
                             </div>
                            
                             <div className="right">
                             
-                            <p><span className="heading" style={{color:'orange'}}><i className="wi wi-sunrise is-size-5"/> sunrise: </span>{this.props.sunrise?<span>{this.props.sunrise}</span>:<span><i className="wi wi-na is-size-4"/></span>}</p>
+                            <p><span className="heading" style={{color:'orange'}}><i className="wi wi-sunrise is-size-5"/> sunrise: </span>{this.props.sunrise?<span>{this.props.sunrise}</span>:<span><i className="wi wi-na is-size-4 blank"/></span>}</p>
 
-                            <p><span className="heading" style={{color:'darkblue'}}><i className="wi wi-moonrise is-size-5"/> sunset: </span>{this.props.sunset?<span>{this.props.sunset}</span>:<span><i className="wi wi-na is-size-4"/></span>}</p>
+                            <p><span className="heading" style={{color:'darkblue'}}><i className="wi wi-moonrise is-size-5"/> sunset: </span>{this.props.sunset?<span>{this.props.sunset}</span>:<span><i className="wi wi-na is-size-4 blank"/></span>}</p>
                            
                             </div>
                         </div>
@@ -162,6 +168,8 @@ export class Weather2 extends Component {
     );
   }
 }
+
+
 
 function error(){
     return(
